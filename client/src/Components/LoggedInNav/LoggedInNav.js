264f783c0afbefb20d6b19b2655fcb795b28a2ec
David Wu <davidjwu92@gmail.com>
2020-03-25 16:11:06 -0700

    Changed Multiplayer option to Leaderboards

--- client/src/Components/LoggedInNav/LoggedInNav.js
+++ client/src/Components/LoggedInNav/LoggedInNav.js
@@ -17,7 +17,7 @@ const LoggedinNav = () => {
       <div className="nav-content">
         <ul className="tabs tabs-transparent">
           <li id="hovEffect" className="tab left"><Link to="/speedscrabble">New Game <i className="fas fa-pencil-alt tiny"></i></Link></li>
-          <li id="hovEffect" className="tab left"><Link to="/multiplayer">Multiplayer <i className="fas fa-pencil-alt tiny"></i></Link></li>
+          <li id="hovEffect" className="tab left"><Link to="/leaderboards">Leaderboards <i className="fas fa-medal tiny"></i></Link></li>
           <li id="hovEffect" className="tab"><Link to="/help">Help <i className="fas fa-question-circle tiny"></i></Link></li>
           <li id="hovEffect" className="tab"><Link to="/test">Test Page</Link></li>
           <li id="hovEffect" className="tab right" onClick={logout}><Link to="/"><i className="fas fa-sign-out-alt"></i></Link></li>
---
+++ client/src/Pages/Leaderboards/Leaderboards.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const Leaderboards = () => {
+  return(
+    <h1>Leaderboards Page</h1>
+  )
+}
+export default Leaderboards
